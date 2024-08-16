c7b49869c3a2d9dc0ccd2451496f8b92d6965015
Shreyajain <shreya.jain@squareops.com>
2024-08-16 11:15:17 +0000

    AD-336|PGL Enhancement

--- main.tf
+++ main.tf
@@ -1079,12 +1079,12 @@ resource "kubernetes_config_map" "loki_dashboard" {
   }
 
   data = {
-    "loki-dashboard.json" = "${file("${path.module}/grafana/dashboards/loki.json")}",
-    "full-loki-dashboard.json" = "${file("${path.module}/grafana/dashboards/Full_loki_logs.json")}",
-    "5xx.json" =  "${file("${path.module}/grafana/dashboards/5xx.json")}",
-    "4xx.json" =  "${file("${path.module}/grafana/dashboards/4xx.json")}",
-    "3xx.json" =  "${file("${path.module}/grafana/dashboards/3xx.json")}",
-    "2xx.json" =  "${file("${path.module}/grafana/dashboards/2xx.json")}",
+    "loki-dashboard.json" = "${file("${path.module}/grafana/dashboards/loki.json")}"
+    # "full-loki-dashboard.json" = "${file("${path.module}/grafana/dashboards/Full_loki_logs.json")}",
+    # "5xx.json" =  "${file("${path.module}/grafana/dashboards/5xx.json")}",
+    # "4xx.json" =  "${file("${path.module}/grafana/dashboards/4xx.json")}",
+    # "3xx.json" =  "${file("${path.module}/grafana/dashboards/3xx.json")}",
+    # "2xx.json" =  "${file("${path.module}/grafana/dashboards/2xx.json")}",
   }
 }
 
