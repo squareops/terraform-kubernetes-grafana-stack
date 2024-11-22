1ebd283a47cfa0b16c9e1d15a001399e3121213e
ankush-sqops <ankush.upadhyay@squareops.com>
2024-11-22 15:42:20 +0530

    Release 3.0.3 (#75)
    * upgraded module with enhanced feature and bug fixes
    
    * Fixed loki-scalable issue and Updated loki-scalable and mimir helm chart version (#61)
    
    Co-authored-by: vinayakops <vinayak.gautam@revnue.com>
    
    * fixed promtail issue (#62)
    
    Co-authored-by: vinayakops <vinayak.gautam@revnue.com>
    
    * Updated promtail values.yaml file (#63)
    
    * fixed promtail issue
    
    * updated promtail config
    
    ---------
    
    Co-authored-by: vinayakops <vinayak.gautam@revnue.com>
    
    * Update main.tf
    
    * Updated code for phase 2
    
    * AD-336|PGL Enhancement
    
    * AD-336|PGL Enhancement
    
    * Update output.tf
    
    * New requirements (#69)
    
    * New requirements added
    
    * New requirements added
    
    * Lki changes
    
    * Dashboard Fixes (#71)
    
    * New requirements added
    
    * New requirements added
    
    * Lki changes
    
    * Dashboard fixes
    
    * AD-336| Nginx related dashboard changes| Promatil new lables add| Old dashboards cleaned
    
    * Cost related tag for aws resources
    
    * updated source path in examples and readme
    
    * update source and version
    
    * ALB/NLB Support
    
    ---------
    
    Co-authored-by: vinayakops <vinayak.gautam@revnue.com>
    Co-authored-by: yuvraj-squareops1 <121240910+yuvraj-squareops1@users.noreply.github.com>
    Co-authored-by: vinayakgautamops <95210787+vinayakgautamops@users.noreply.github.com>
    Co-authored-by: Shreyajain <shreya.jain@squareops.com>
    Co-authored-by: Monachawla1712 <chawalamona44@gmail.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,16 +3,17 @@ locals {
   region      = ""
   environment = ""
   additional_tags = {
-    Owner      = "organization_name"
-    Expires    = "Never"
-    Department = "Engineering"
-    Product    = "Atmosly"
+    Owner       = "organization_name"
+    Expires     = "Never"
+    Department  = "Engineering"
+    Product     = ""
     Environment = local.environment
   }
 }
 
 module "pgl" {
-  source                        = "git@github.com:sq-ia/terraform-kubernetes-grafana.git"
+  source                        = "squareops/grafana-stack/kubernetes"
+  version                       = "3.0.3"
   cluster_name                  = ""
   kube_prometheus_stack_enabled = true
   loki_enabled                  = false
@@ -32,8 +33,10 @@ module "pgl" {
     grafana_enabled                     = true
     prometheus_hostname                 = "prometheus.com"
     prometheus_internal_ingress_enabled = false
-    grafana_ingress_load_balancer       = "nlb" ##Choose your load balancer type (e.g., NLB or ALB). If using ALB, ensure you provide the ACM certificate ARN for SSL.
-    alb_acm_certificate_arn             = ""    #"arn:aws:acm:${local.region}:444455556666:certificate/certificate_ID"
+    grafana_ingress_load_balancer       = "nlb"   ##Choose your load balancer type (e.g., NLB or ALB). If using ALB, ensure you provide the ACM certificate ARN for SSL.
+    ingress_class_name                  = "nginx" # enter ingress class name according to your requirement (example: "nginx", "internal-ingress", "private-nginx")
+    alb_acm_certificate_arn             = ""      #"arn:aws:acm:${local.region}:444455556666:certificate/certificate_ID"
+    private_alb_enabled                 = false   # Set to true, when wanted to deploy PGL on ALB internal
     loki_internal_ingress_enabled       = false
     loki_hostname                       = "loki.com"
     mimir_s3_bucket_config = {
